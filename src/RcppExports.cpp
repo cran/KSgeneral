52db6a96830851180df295b2c2747d107adb5091
Senren Tan <raymondtsrtsr@outlook.com>
2021-12-05 15:00:05 +0000

    version 1.1.0

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // ks_c_cdf_Rcpp
 double ks_c_cdf_Rcpp(double n);
 RcppExport SEXP _KSgeneral_ks_c_cdf_Rcpp(SEXP nSEXP) {
